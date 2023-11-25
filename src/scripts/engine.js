82fe16949df6806b6c344bbcab25630b4fffd8e2
Renan Menezes <2003rcsm@gmail.com>
2023-11-25 00:00:49 -0300

    Criando os cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,30 @@
+const emojis = [
+    "🤣",
+    "🤣",
+    "✔",
+    "✔",
+    "👀",
+    "👀",
+    "🎁",
+    "🎁",
+    "🤔",
+    "🤔",
+    "🐱‍👤",
+    "🐱‍👤",
+    "✨",
+    "✨",
+    "🎉",
+    "🎉",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1 ));
+
+for(let i = 0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
